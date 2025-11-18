ff94fb07b9e8b0e5d5700c7478937cce022feaa9
Andrea Grimandi <andrea.grimandi1@protonmail.ch>
2025-11-18 09:00:54 +0100

    Change app run port from 5000 to 4986

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ if __name__ == '__main__':
     app = ApplicationController()
     app.setup()
 
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=4986, debug=True)
